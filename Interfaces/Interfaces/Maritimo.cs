0fc7934168ea8fb345b650050fbd51ac40b2c9fa
aitorsaratxo <aitsaruri@gmail.com>
2022-04-01 14:29:01 +0200

    Primer vistazo a interfaces en C#

---
+++ Interfaces/Interfaces/Maritimo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal abstract class Maritimo : Vehiculo, I_Flotable
+    {
+        public abstract void Flotar();
+    }
+}
---
+++ Interfaces/Interfaces/Maritimo1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal class Maritimo1 : Maritimo
+    {
+        public override void Flotar()
+        {
+            Console.WriteLine("Vehiculo flotando...");
+        }
+    }
+}
